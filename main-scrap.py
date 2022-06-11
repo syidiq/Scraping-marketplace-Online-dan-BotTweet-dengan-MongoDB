5813ee9d78979d2c7fe3344271063b39da77298f
Muhammad Syidiq Abdjanur <41407658+syidiq@users.noreply.github.com>
2022-06-11 11:00:32 +0700

    Update main-scrap.py

--- main-scrap.py
+++ main-scrap.py
@@ -8,16 +8,17 @@ import requests
 import json
 import pandas as pd
 import re
+import os
 
 # Iport Deploy
 import Deploy_detail_product
 import Deploy_list_product
 
 # Coneksi ke data MongoDB
-username = "syidiq"
-password = "syidiq123"
-cluster = "clustersyidiq"
-code = "3rst0"
+username = os.getenv(USERNAME_MONGODB)
+password = os.getenv(PASSWORD_MONGODB)
+cluster = os.getenv(CLUSTER_MONGODB)
+code = os.getenv(CODE_MONGODB)
 uri = ("mongodb+srv://{}:{}@{}.{}.mongodb.net/?retryWrites=true&w=majority").format(username,password,cluster,code)
 # start client to connect to MongoDB server 
 client = MongoClient(uri)
